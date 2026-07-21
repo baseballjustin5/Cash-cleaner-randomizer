53b822dc91aaf6ba5767b8b08db8b8345560a333
Justin <baseballjustin5@users.noreply.github.com>
2026-07-21 06:04:38 -0500

    added Coins, Rolled Coins, and Wrapped Rolled Coins (blocks) as checks

--- scripts/validationRule.lua
+++ scripts/validationRule.lua
@@ -23,15 +23,34 @@ ValidationRules = {
     ["blocks-usd"] = true,
     ["blocks-jpy"] = true,
     ["blocks-eur"] = true,
+    ["blocks-gbp"] = true,
+
+    ["blocks-coin-usd"] = true,
+    ["blocks-coin-jpy"] = true,
+    ["blocks-coin-eur"] = true,
+    ["blocks-coin-gbp"] = true,
 
     ["packs-jpy"] = true,
     ["packs-usd"] = true,
     ["packs-eur"] = true,
+    ["packs-gbp"] = true,
+
+    ["rolls-coin-usd"] = true,
+    ["rolls-coin-jpy"] = true,
+    ["rolls-coin-eur"] = true,
+    ["rolls-coin-gbp"] = true,
 
     ["bills-eur"] = true,
     ["bills-jpy"] = true,
     ["bills-usd"] = true,
+    ["bills-gbp"] = true,
 
+    ["coins-usd"] = true,
+    ["coins-jpy"] = true,
+    ["coins-eur"] = true,
+    ["coins-gbp"] = true,
+
+    -- Euro Notes
     ["blocks-eur100"] = true,
     ["blocks-eur50"] = true,
     ["blocks-eur20"] = true,
@@ -44,6 +63,7 @@ ValidationRules = {
     ["bills-eur50"] = true,
     ["bills-eur20"] = true,
 
+    -- US Dollar Bills
     ["blocks-usd100"] = true,
     ["blocks-usd50"] = true,
     ["blocks-usd20"] = true,
@@ -59,6 +79,7 @@ ValidationRules = {
     ["bills-usd20"] = true,
     ["bills-usd10"] = true,
 
+    -- Japanese Yen Notes
     ["blocks-jpy10000"] = true,
     ["blocks-jpy5000"] = true,
     ["blocks-jpy1000"] = true,
@@ -70,4 +91,84 @@ ValidationRules = {
     ["bills-jpy10000"] = true,
     ["bills-jpy5000"] = true,
     ["bills-jpy1000"] = true,
+
+    -- British Pound Notes
+    ["blocks-gbp100"] = true,
+    ["blocks-gbp50"] = true,
+    ["blocks-gbp20"] = true,
+    ["blocks-gbp10"] = true,
+
+    ["packs-gbp100"] = true,
+    ["packs-gbp50"] = true,
+    ["packs-gbp20"] = true,
+    ["packs-gbp10"] = true,
+
+    ["bills-gbp100"] = true,
+    ["bills-gbp50"] = true,
+    ["bills-gbp20"] = true,
+    ["bills-gbp10"] = true,
+
+    -- Euro Coins
+    ["blocks-coins-eur2.00"] = true,
+    ["blocks-coins-eur1.00"] = true,
+    ["blocks-coins-eur0.50"] = true,
+    ["blocks-coins-eur0.20"] = true,
+
+    ["rolls-coins-eur2.00"] = true,
+    ["rolls-coins-eur1.00"] = true,
+    ["rolls-coins-eur0.50"] = true,
+    ["rolls-coins-eur0.20"] = true,
+
+    ["coins-eur2.00"] = true,
+    ["coins-eur1.00"] = true,
+    ["coins-eur0.50"] = true,
+    ["coins-eur0.20"] = true,
+
+    -- US Dollar Coins
+    ["blocks-coins-usd1.00"] = true,
+    ["blocks-coins-usd0.50"] = true,
+    ["blocks-coins-usd0.25"] = true,
+    ["blocks-coins-usd0.10"] = true,
+
+    ["rolls-coins-usd1.00"] = true,
+    ["rolls-coins-usd0.25"] = true,
+    ["rolls-coins-eur0.50"] = true,
+    ["rolls-coins-eur0.20"] = true,
+
+    ["coins-usd1.00"] = true,
+    ["coins-usd0.50"] = true,
+    ["coins-usd0.25"] = true,
+    ["coins-usd0.10"] = true,
+
+    -- Japanese Yen Coins
+    ["blocks-coins-jpy10"] = true,
+    ["blocks-coins-jpy50"] = true,
+    ["blocks-coins-jpy100"] = true,
+    ["blocks-coins-jpy500"] = true,
+
+    ["rolls-coins-jpy10"] = true,
+    ["rolls-coins-jpy50"] = true,
+    ["rolls-coins-jpy100"] = true,
+    ["rolls-coins-jpy500"] = true,
+
+    ["coins-jpy10"] = true,
+    ["coins-jpy50"] = true,
+    ["coins-jpy100"] = true,
+    ["coins-jpy500"] = true,
+
+    -- British Pount Coins
+    ["blocks-coins-gbp2.00"] = true,
+    ["blocks-coins-gbp1.00"] = true,
+    ["blocks-coins-gbp0.50"] = true,
+    ["blocks-coins-gbp0.20"] = true,
+
+    ["rolls-coins-gbp2.00"] = true,
+    ["rolls-coins-gbp1.00"] = true,
+    ["rolls-coins-gbp0.50"] = true,
+    ["rolls-coins-gbp0.20"] = true,
+
+    ["coins-gbp2.00"] = true,
+    ["coins-gbp1.00"] = true,
+    ["coins-gbp0.50"] = true,
+    ["coins-gbp0.20"] = true,
 }
